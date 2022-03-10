d99936547a4b2fdd0551e4ccb0a6978770a0077d
Gianni Cerri <giannicerri@gmail.com>
2022-03-10 16:01:30 -0500

    add popup/options, organize files

---
+++ recipescroller/options/options.js
@@ -0,0 +1,1 @@
+//js for options.html page
--- recipescroller/popup/popup.js
+++ recipescroller/popup/popup.js
@@ -1,17 +1,21 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+//js for popup.html page
 
+// Initialize button
+let findRecipe = document.getElementById("findRecipe");
+
+/*
 chrome.storage.sync.get("color", ({ color }) => {
     changeColor.style.backgroundColor = color;
 });
+*/
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+// When the button is clicked, run content.js on current page
+findRecipe.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: setPageBackgroundColor,
+        files: ['content.js'],
     });
 });
 
